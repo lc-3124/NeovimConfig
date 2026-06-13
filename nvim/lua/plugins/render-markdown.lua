128310ce87ff30dbb97708be17f70bb67c28b1d5
lc3124 <lc3124@aliyun.com>
2026-06-13 09:53:58 +0800

    feat: 添加 render-markdown.nvim 支持表格/代码块/checkbox 渲染

---
+++ nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,12 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  opts = {
+    table = { enabled = true },
+    code = { enabled = true },
+    checkbox = { enabled = true },
+    heading = { enabled = true },
+    bullet = { enabled = true },
+  },
+  ft = { "markdown", "Avante" },
+  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+}
