ecbe8e5ddd75dbcd8c06a67e2a0a567b4c32cd72
lc3124 <lc3124@users.noreply.github.com>
2026-08-20 18:51:28 +0800

    backup: 2026-08-20 18:51 配置快照 + 包清单

--- docs/aur_install.sh
+++ docs/aur_install.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
-# 自动生成于 2026-08-10 21:43，勿手改；来源: docs/aur-packages.txt
+# 自动生成于 2026-08-20 18:51，勿手改；来源: docs/aur-packages.txt
 # 用法: ./docs/aur_install.sh   # 安装全部 AUR 包（--needed 已装则跳过）
 
+paru -S activate-linux --needed --noconfirm ;
 paru -S adw-gtk-theme-git --needed --noconfirm ;
 paru -S android-sdk-platform-tools --needed --noconfirm ;
 paru -S animeko-appimage --needed --noconfirm ;
+paru -S baidunetdisk-bin --needed --noconfirm ;
 paru -S bibata-cursor-theme --needed --noconfirm ;
 paru -S catppuccin-sddm-theme-frappe --needed --noconfirm ;
 paru -S cbonsai --needed --noconfirm ;
 paru -S classin-bin --needed --noconfirm ;
+paru -S com.qq.weixin.work.deepin --needed --noconfirm ;
 paru -S dxvk-bin --needed --noconfirm ;
 paru -S fcitx5-anytalk --needed --noconfirm ;
+paru -S hypr-kdeconnect-fix-git --needed --noconfirm ;
+paru -S hyprland-qt-support-debug --needed --noconfirm ;
 paru -S input-remapper-bin --needed --noconfirm ;
 paru -S jhentai-bin --needed --noconfirm ;
+paru -S kvantum-theme-catppuccin-git --needed --noconfirm ;
+paru -S lib32-giflib --needed --noconfirm ;
 paru -S libsoup --needed --noconfirm ;
 paru -S linuxqq-nt --needed --noconfirm ;
 paru -S listen1-desktop-appimage --needed --noconfirm ;
@@ -37,3 +44,5 @@ paru -S tokyonight-gtk-theme-git --needed --noconfirm ;
 paru -S ttf-material-symbols-variable-git --needed --noconfirm ;
 paru -S wayle-bin --needed --noconfirm ;
 paru -S wechat-appimage --needed --noconfirm ;
+paru -S wemeet-bin --needed --noconfirm ;
+paru -S xdg-desktop-portal-termfilechooser-hunkyburrito-git --needed --noconfirm ;
